94d88810c80e4707a720be3e10fd9bf8187739aa
Matt Morrissette <yinzara@gmail.com>
2023-02-09 23:44:47 -0800

    chore: simplify and add renovatebot

--- cli.js
+++ cli.js
@@ -3,5 +3,6 @@
 const child_process = require("child_process");
 const PLUGIN = require("./");
 
-child_process.spawn(PLUGIN, process.argv.slice(2), {stdio: 'inherit'})
-  .on('exit', code => process.exit(code));
+child_process
+  .spawn(PLUGIN, process.argv.slice(2), { stdio: "inherit" })
+  .on("exit", (code) => process.exit(code));
--- post-install.js
+++ post-install.js
@@ -1,38 +1,76 @@
-const path = require('path');
-const AdmZip = require('adm-zip');
+const AdmZip = require("adm-zip");
+const fs = require("fs");
+const https = require("https");
+const path = require("path");
+const PLUGIN = require("./");
 
-const fs = require('fs');
-const download = require('download');
-const PLUGIN = require('./');
-const { version } = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf-8')); // read the version from the package.json
+const { version } = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "package.json"), "utf-8")
+); // read the version from the package.json
 
-const DL_PREFIX = 'https://github.com/protocolbuffers/protobuf-javascript/releases/download/v';
-const BIN_DIR = path.resolve(__dirname, 'bin');
-const EXT = process.platform === 'win32' ? '.exe' : '';
-const PLATFORM_NAME = process.platform === 'win32' ? '' : process.platform === 'darwin' ? 'osx-' : 'linux-';
-const ARCH = process.platform === 'win32' ? process.arch === 'ia32' ? 'win32' : 'win64' :
-            process.arch === 'ppc64' ? 'ppcle_64' :
-            process.arch === 'arm64' ? 'aarch_64' :
-            process.arch === 's390x' ? 's390_64' :
-            process.arch === 'ia32' ? 'x86_32' : 'x86_64';
+const DL_PREFIX =
+  "https://github.com/protocolbuffers/protobuf-javascript/releases/download/v";
+const BIN_DIR = path.resolve(__dirname, "bin");
+const EXT = process.platform === "win32" ? ".exe" : "";
+const PLATFORM_NAME =
+  process.platform === "win32"
+    ? "win"
+    : process.platform === "darwin"
+    ? "osx-"
+    : "linux-";
+const ARCH =
+  process.platform === "win32"
+    ? process.arch === "ia32"
+      ? "32"
+      : "64"
+    : process.arch === "ppc64"
+    ? "ppcle_64"
+    : process.arch === "arm64"
+    ? "aarch_64"
+    : process.arch === "s390x"
+    ? "s390_64"
+    : process.arch === "ia32"
+    ? "x86_32"
+    : "x86_64";
+
+const resHandler = (resolve, reject, res) => {
+  if (res.statusCode === 302) {
+    https
+      .get(res.headers.location, (res2) => resHandler(resolve, reject, res2))
+      .on("error", reject);
+  } else {
+    const data = [];
+    res
+      .on("data", (chunk) => {
+        data.push(chunk);
+      })
+      .on("end", () => {
+        resolve(Buffer.concat(data));
+      });
+  }
+};
 
 async function run() {
-  if (process.arch === 'ppc' || process.arch === 'arm' || process.arch === 'mips' || process.arch === 'mipsel' || process.arch === 's390') {
-    throw new Error(
-      `Unsupported arch: ${process.arch}`
-    );
+  if (
+    process.arch === "ppc" ||
+    process.arch === "arm" ||
+    process.arch === "mips" ||
+    process.arch === "mipsel" ||
+    process.arch === "s390"
+  ) {
+    throw new Error(`Unsupported arch: ${process.arch}`);
   }
 
-  if (!fs.existsSync(BIN_DIR))
-    fs.mkdirSync(BIN_DIR)
+  if (!fs.existsSync(BIN_DIR)) fs.mkdirSync(BIN_DIR);
   const zipFilename = `protobuf-javascript-${version}-${PLATFORM_NAME}${ARCH}.zip`;
 
-  const downloadUrl = DL_PREFIX + version + '/' + zipFilename;
+  const downloadUrl = DL_PREFIX + version + "/" + zipFilename;
 
-  console.log('Downloading', downloadUrl);
-  const buffer = await download(downloadUrl).catch(err => {
-    console.error(err.message);
-    process.exit(1);
+  console.log("Downloading", downloadUrl);
+  const buffer = await new Promise((resolve, reject) => {
+    https
+      .get(downloadUrl, (res) => resHandler(resolve, reject, res))
+      .on("error", reject);
   });
 
   const exeFilename = `bin/protoc-gen-js${EXT}`;
@@ -43,12 +81,12 @@ async function run() {
     false,
     true,
     false,
-    path.basename(PLUGIN));
-  fs.chmodSync(PLUGIN, '0755');
+    path.basename(PLUGIN)
+  );
+  fs.chmodSync(PLUGIN, "0755");
 }
 
-run().catch(e => {
-  console.error(e)
-  process.exit(1)
+run().catch((e) => {
+  console.error(e);
+  process.exit(1);
 });
-
